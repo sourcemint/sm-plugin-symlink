024f1f2a2b9ea31ccfb04e58b1eacf1a3116b539
cadorn <christoph@christophdorn.com>
2013-02-21 14:02:27 -0800

    initial commit

---
+++ plugin.js
@@ -0,0 +1,50 @@
+
+const PATH = require("path");
+const URL = require("url");
+
+
+exports.for = function(API, plugin) {
+
+    plugin.resolveLocator = function(locator, options, callback) {
+        var self = this;
+
+        if (!locator.url) {
+            locator.url = locator.descriptor.pointer;
+        }
+
+        locator.getLocation = function(type) {
+            var locations = {
+                "pointer": locator.url
+            };
+            locations.symlink = locator.url;
+            return (type)?locations[type]:locations;
+        }
+
+        return callback(null, locator);
+    }
+
+    plugin.download = function(uri, options, callback) {
+
+        var parsedUri = URL.parse(uri);
+
+        if (!API.FS.existsSync(parsedUri.pathname)) {
+            return callback(null, {
+                status: 404,
+                cachePath: parsedUri.pathname
+            });
+        }
+        return callback(null, {
+            status: 200,
+            cachePath: parsedUri.pathname
+        });
+    }
+
+    plugin.extract = function(fromPath, toPath, locator, options) {
+        if (!API.FS.existsSync(PATH.dirname(toPath))) {
+            API.FS.mkdirsSync(PATH.dirname(toPath));
+        }
+        FS.symlinkSync(fromPath, toPath);
+        return API.Q.resolve();
+    }
+
+}
